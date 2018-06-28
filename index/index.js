21c7bfb7f7b66b588c5fa2680e89daf84ed5f0ca
nansanhao <389746614@qq.com>
2018-06-28 17:54:35 +0800

    热门专辑

--- index/index.js
+++ index/index.js
@@ -13,6 +13,14 @@ new Vue({
         menu_active_index: '1',
         carousel_items: [
             {
+                image_url: "http://p1.music.126.net/tRIh-Ur8BZBTS0vbfmZ0Nw==/109951163379832546.jpg",
+                link_url: ""
+            },
+            {
+                image_url: "http://p1.music.126.net/sQdU5PZeu7YYMU2nHj5L3A==/109951163379967707.jpg",
+                link_url: ""
+            },
+            {
                 image_url: "http://p1.music.126.net/kCpE47CGXd79Osh-GvWwzQ==/109951163377208783.jpg",
                 link_url: ""
             },
@@ -74,6 +82,51 @@ new Vue({
                 num:"235416",
                 create_date:"2018-06-27 17:12"
             }
+        ],
+        hot_albumlist:[
+            {
+                title:"EVERYTHING IS LOVE",
+                singer:"The Carters / Beyoncé / Jay-Z",
+                img_url:"http://p3.music.126.net/WcVJXehoHv1_b0GyWFCjmA==/109951163365318229.jpg?param=100y100"
+            },
+            {
+                title:"王牌冤家",
+                singer:"李荣浩",
+                img_url:"http://p3.music.126.net/ldRfSHRgtSZRsNruD54taQ==/109951163370797182.jpg?param=100y100"
+            },
+            {
+                title:"平凡的一天",
+                singer:"毛不易",
+                img_url:"http://p4.music.126.net/vmCcDvD1H04e9gm97xsCqg==/109951163350929740.jpg?param=100y100"
+            },
+            {
+                title:"Born To Be Yours",
+                singer:"Kygo / Imagine Dragons",
+                img_url:"http://p3.music.126.net/w84c5_ebsl9cqchrzT1pug==/109951163349702736.jpg?param=100y100"
+            },
+            {
+                title:"F.L.Y. BOYS F.L.Y. GIRLS",
+                singer:"GENERATIONS from EXILE TRIBE",
+                img_url:"http://p4.music.126.net/ucsboPMm818QenSE8g-YnQ==/109951163336294560.jpg?param=100y100"
+            }
+        ],
+        hot_singer:[
+            {
+                name:"张惠妹aMEI",
+                info:"台湾歌手张惠妹",
+                image_url:"http://p1.music.126.net/p9U80ex1B1ciPFa125xV5A==/5931865232210340.jpg?param=62y62"
+            }
         ]
+
+    },
+    methods:{
+        fomatString:function (title,len) {
+            if(title.length>=len){
+                return title.slice(0,len-1)+"..."
+            }else{
+                return title
+            }
+
+        }
     }
 })
