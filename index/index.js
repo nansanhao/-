6efe94f29b196bf43bf265bb4159410b34e80a42
nansanhao <389746614@qq.com>
2018-06-29 00:25:13 +0800

    index页面“

--- index/index.js
+++ index/index.js
@@ -115,6 +115,21 @@ new Vue({
                 name:"张惠妹aMEI",
                 info:"台湾歌手张惠妹",
                 image_url:"http://p1.music.126.net/p9U80ex1B1ciPFa125xV5A==/5931865232210340.jpg?param=62y62"
+            },
+            {
+                name:"Fine乐团",
+                info:"独立音乐人",
+                image_url:"http://p1.music.126.net/1EN_iqQWU_E3DafzEOh3cA==/3302932937408956.jpg?param=62y62"
+            },
+            {
+                name:"萬曉利",
+                info:"民谣歌手、中国现代民谣的代表人物之一",
+                image_url:"http://p1.music.126.net/ZuktZvjcxpYBjcWC3gmbPg==/19027048718765608.jpg?param=62y62"
+            },
+            {
+                name:"音乐人赵雷",
+                info:"民谣歌手",
+                image_url:"http://p1.music.126.net/v_zYgE9kmAwVGWV2c8hFxA==/7943971513291094.jpg?param=62y62"
             }
         ]
 
