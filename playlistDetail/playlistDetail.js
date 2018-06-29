4b9d53381faa407d33a9e6864aec4067f82fb266
nansanhao <389746614@qq.com>
2018-06-29 11:59:15 +0800

    详情页开始做

---
+++ playlistDetail/playlistDetail.js
@@ -0,0 +1,23 @@
+new Vue({
+    el:"#app",
+    data:{
+        isPlaylist:true,
+        playlist:{
+            name:"韵律中邂逅惊艳女嗓，美到心醉",
+            song_count:6324,
+            creat_time:"2018-05-14",
+            introduction:"会有始料不及的运气，\n" +
+            "会有突如其来的欢喜，\n" +
+            "放空的心，是最好的礼物； \n" +
+            "独走的路，是最美的风景；\n" +
+            "好听的女声那么多，\n" +
+            "而我却偏偏喜欢你。\n" +
+            "\n" +
+            "♥这么好听的女声你确定要错过？",
+            creator:{
+                user_name:"",
+                photo:""
+            }
+        }
+    }
+})
