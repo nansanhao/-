30f4114cffb8a4c61d571887641aba898bf0737e
nansanhao <389746614@qq.com>
2018-06-29 17:52:10 +0800

    6月29下午

--- playlistDetail/playlistDetail.js
+++ playlistDetail/playlistDetail.js
@@ -1,23 +1,60 @@
 new Vue({
     el:"#app",
     data:{
+        value3:null,
         isPlaylist:true,
         playlist:{
-            name:"韵律中邂逅惊艳女嗓，美到心醉",
-            song_count:6324,
-            creat_time:"2018-05-14",
-            introduction:"会有始料不及的运气，\n" +
-            "会有突如其来的欢喜，\n" +
-            "放空的心，是最好的礼物； \n" +
-            "独走的路，是最美的风景；\n" +
-            "好听的女声那么多，\n" +
-            "而我却偏偏喜欢你。\n" +
-            "\n" +
-            "♥这么好听的女声你确定要错过？",
-            creator:{
-                user_name:"",
-                photo:""
-            }
+            title:"韵律中邂逅惊艳女嗓，美到心醉",
+            play_count:6324,
+            collect_count:511,
+            download_count:322,
+            create_date:"2018-05-14",
+            img_url:"http://p3.music.126.net/2-04ThxCahYBMbkXzFHwfw==/19161189137853511.jpg?param=200y200",
+            introduction:"五月，会有始料不及的运气，会有突如其来的欢喜，放空的心，是最好的礼物； 独走的路，是最美的风景；好听的女声那么多，而我却偏偏喜欢你。♥这么好听的女声你确定要错过？",
+            author:{
+                name:"南三号",
+                img_url:"http://p1.music.126.net/0btwtKKf0L162zX2WKQbBQ==/109951163379831126.jpg?param=40y40"
+            },
+            tags:["欧美","流行","电子"],
+            songs:[
+                {
+                    title:"Safe",
+                    duration:"03:20",
+                    album:"Safe",
+                    singer:"Daya"
+                },
+                {
+                    title:"Only You",
+                    duration:"03:09",
+                    album:"Only You",
+                    singer:"Cheat Codes"
+                },{
+                    title:"You",
+                    duration:"03:20",
+                    album:"Odyssey",
+                    singer:"Matlda"
+                },{
+                    title:"Need You Right Now",
+                    duration:"03:25",
+                    album:"Safe",
+                    singer:"Hedegaard"
+                },{
+                    title:"Safe",
+                    duration:"03:20",
+                    album:"Safe",
+                    singer:"Daya"
+                },{
+                    title:"Safe",
+                    duration:"03:20",
+                    album:"Safe",
+                    singer:"Daya"
+                },{
+                    title:"Safe",
+                    duration:"03:20",
+                    album:"Safe",
+                    singer:"Daya"
+                }
+            ]
         }
     }
 })
